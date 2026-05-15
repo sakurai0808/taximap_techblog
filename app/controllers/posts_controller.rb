25cbf89cea4b8b759de78b6ff35b47d4e0ca994b
sakurai0808 <sakuraiwork0808@gmail.com>
2026-05-15 20:03:28 +0900

    update: 記事の並び順を降順にする

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    # 降順にする
+    @posts = Post.all.order(created_at: :desc)
   end
 
   # GET /posts/1 or /posts/1.json
